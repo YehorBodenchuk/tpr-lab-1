75e0fb5b9b34a944075c5e86429760b8a2313ec4
YehorBodenchuk <yehor.bodenchuk-pastukhov@sytoss.com>
2024-04-16 13:02:29 +0300

    Refactor tests

--- src/test/java/mars/rover/MarsRoverTest.java
+++ src/test/java/mars/rover/MarsRoverTest.java
@@ -1,30 +1,55 @@
 package mars.rover;
 
 import mars.rover.enums.CardinalDirection;
+import mars.rover.enums.Direction;
 import mars.rover.impls.RoverImpl;
 import mars.rover.intarfaces.Rover;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+
 class MarsRoverTest {
 
-    @Test
-    public void
-    acceptance_test_1() {
-        Rover rover = new RoverImpl(1, 2, CardinalDirection.getByLiteral('N'));
+    private Rover rover;
 
-        String newPosition = rover.start("LMLMLMLMM");
-        assertEquals("1 3 N", newPosition);
+    @BeforeEach
+    public void setUp() {
+        rover = new RoverImpl(1, 2, CardinalDirection.NORTH);
     }
 
     @Test
-    public void
-    acceptance_test_2() {
-        Rover rover = new RoverImpl(3, 3, CardinalDirection.getByLiteral('E'));
+    public void testMoveForward() {
+        rover.moveForward();
 
-        String newPosition = rover.start("MMRMMRMRRM");
-        assertEquals("5 1 E", newPosition);
+        assertEquals("1 3 N", rover.getPosition());
     }
 
+    @Test
+    public void testTurnLeft() {
+        rover.turn(Direction.LEFT);
+
+        assertEquals("1 2 W", rover.getPosition());
+    }
+
+    @Test
+    public void testTurnRight() {
+        rover.turn(Direction.RIGHT);
+
+        assertEquals("1 2 E", rover.getPosition());
+    }
+
+    @Test
+    public void testGetPosition() {
+        assertEquals("1 2 N", rover.getPosition());
+    }
+
+    @Test
+    public void testStart() {
+        String instructions = "LMLMLMLMM";
+
+        String position = rover.start(instructions);
+        assertEquals("1 3 N", position);
+    }
 }
